0ab5e48105b4aec6e324d0d6a3395714272854f3
Coto <programmer.coto.1988@gmail.com>
2018-08-01 23:31:21 -0400

    update irqs

--- src/common/hardware/InterruptsARMCores_c.c
+++ src/common/hardware/InterruptsARMCores_c.c
@@ -61,9 +61,8 @@ void IRQInit(){
 	}
 	
 	REG_IE = 0;
-	
 	REG_IPC_SYNC = 0;
-    REG_IPC_FIFO_CR = RECV_FIFO_IPC_IRQ  | SEND_FIFO_IPC_IRQ | FIFO_IPC_ENABLE;
+    REG_IPC_FIFO_CR = RECV_FIFO_IPC_IRQ  | FIFO_IPC_ENABLE;
 	
 	//set up ppu: do irq on hblank/vblank/vcount/and vcount line is 159
     REG_DISPSTAT = REG_DISPSTAT | DISP_HBLANK_IRQ | DISP_VBLANK_IRQ | (DISP_YTRIGGER_IRQ | (VCOUNT_LINE_INTERRUPT << 15));
@@ -71,11 +70,11 @@ void IRQInit(){
 	uint32 interrupts_to_wait_armX = 0;
 	
 	#ifdef ARM7
-	interrupts_to_wait_armX = interrupts_to_wait_arm7 = IRQ_TIMER1 | IRQ_HBLANK | IRQ_VBLANK | IRQ_VCOUNT | IRQ_RECVFIFO_NOT_EMPTY  | IRQ_SENDFIFO_EMPTY;
+	interrupts_to_wait_armX = interrupts_to_wait_arm7 = IRQ_TIMER1 | IRQ_HBLANK | IRQ_VBLANK | IRQ_VCOUNT | IRQ_RECVFIFO_NOT_EMPTY ;
 	#endif
 	
 	#ifdef ARM9
-	interrupts_to_wait_armX = interrupts_to_wait_arm9 = IRQ_HBLANK| IRQ_VBLANK | IRQ_VCOUNT | IRQ_RECVFIFO_NOT_EMPTY | IRQ_SENDFIFO_EMPTY;
+	interrupts_to_wait_armX = interrupts_to_wait_arm9 = IRQ_HBLANK| IRQ_VBLANK | IRQ_VCOUNT | IRQ_RECVFIFO_NOT_EMPTY;
 	#endif
 	
 	REG_IE = interrupts_to_wait_armX; 
@@ -91,28 +90,36 @@ void IRQInit(){
 #ifdef ARM9
 __attribute__((section(".itcm")))
 #endif
+__attribute__((aligned(4)))
 void NDS_IRQHandler(){
-	
-	uint32 REG_IE_SET = REG_IF & REG_IE;
-	
-	// 2/2
-	REG_IE_SET |= SWI_CHECKBITS;
-	
-	#ifdef ARM7
-	//arm7 wifi cart irq
-	if(REG_IE_SET & IRQ_WIFI){
-		Wifi_Interrupt();
-		REG_IF = IRQ_WIFI;
+	uint32 REG_IE_SET = (REG_IE & REG_IF);
+	if(REG_IE_SET & IRQ_HBLANK){
+		HblankUser();
+		SWI_CHECKBITS = REG_IF = IRQ_HBLANK;
+	}
+	if(REG_IE_SET & IRQ_VBLANK){
+		#ifdef ARM7
+		doSPIARM7IO();
+		Wifi_Update();
+		#endif
+		
+		#ifdef ARM9
+		//handles DS-DS Comms
+		sint32 currentDSWNIFIMode = doMULTIDaemonStage1();
+		#endif
+		
+		//key event between frames
+		do_keys();
+		VblankUser();
+		SWI_CHECKBITS = REG_IF = IRQ_VBLANK;
 	}
 	
-	//clock //could cause freezes
-	if(REG_IE_SET & IRQ_RTCLOCK){
-		REG_IF = IRQ_RTCLOCK;
+	if(REG_IE_SET & IRQ_VCOUNT){
+		VcounterUser();
+		SWI_CHECKBITS = REG_IF = IRQ_VCOUNT;
 	}
-	#endif
 	
 	////			Common
-	
 	if(REG_IE_SET & IRQ_TIMER0){
 		Timer0handlerUser();
 		REG_IF = IRQ_TIMER0;
@@ -139,38 +146,6 @@ void NDS_IRQHandler(){
 		Timer3handlerUser();
 		REG_IF = IRQ_TIMER3;
 	}
-	
-	if(REG_IE_SET & IRQ_HBLANK){
-		HblankUser();
-		REG_IF = IRQ_HBLANK;
-	}
-	
-	if(REG_IE_SET & IRQ_VBLANK){
-		
-		#ifdef ARM7
-		Wifi_Update();
-		#endif
-		
-		#ifdef ARM9
-		//handles DS-DS Comms
-		sint32 currentDSWNIFIMode = doMULTIDaemonStage1();
-		#endif
-		
-		//key event between frames
-		do_keys();
-		VblankUser();
-		REG_IF = IRQ_VBLANK;
-	}
-	
-	if(REG_IE_SET & IRQ_VCOUNT){
-		#ifdef ARM7
-		doSPIARM7IO();
-		#endif
-		VcounterUser();
-		REG_IF = IRQ_VCOUNT;
-	}
-	
-	
 	if(REG_IE_SET & IRQ_SENDFIFO_EMPTY){
 		HandleFifoEmpty();
 		REG_IF = IRQ_SENDFIFO_EMPTY;
@@ -181,9 +156,18 @@ void NDS_IRQHandler(){
 		REG_IF=IRQ_RECVFIFO_NOT_EMPTY;
 	}
 	
-	//Update BIOS flags
-	SWI_CHECKBITS = REG_IF;
+	#ifdef ARM7
+	//arm7 wifi cart irq
+	if(REG_IE_SET & IRQ_WIFI){
+		Wifi_Interrupt();
+		REG_IF = IRQ_WIFI;
+	}
 	
+	//clock //could cause freezes
+	if(REG_IE_SET & IRQ_RTCLOCK){
+		REG_IF = IRQ_RTCLOCK;
+	}
+	#endif
 }
 
 
