c14cb4915fb21d459742e3442c7c706c5b781283
Coto <programmer.coto.1988@gmail.com>
2022-01-14 11:13:38 -0300

    fix

--- lib/Woopsi/woopsiExamples/demoWoopsi/arm9/source/main.cpp
+++ lib/Woopsi/woopsiExamples/demoWoopsi/arm9/source/main.cpp
@@ -120,7 +120,6 @@ int main(int argc, char **argv) {
 		printf("FS Init error: %d", ret);
 	}
 	
-	switch_dswnifi_mode(dswifi_idlemode);
 	asm("mcr	p15, 0, r0, c7, c10, 4");
 	flush_icache_all();
 	flush_dcache_all();
