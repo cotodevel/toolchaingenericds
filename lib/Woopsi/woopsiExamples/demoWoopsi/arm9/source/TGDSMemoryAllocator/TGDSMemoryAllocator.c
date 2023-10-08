cda0c596c9f56c324e6a079185432216d0e5f5a0
cotodevel <programmer.coto.1988@gmail.com>
2023-10-08 12:48:16 -0300

    WoopsiSDK Demo now building directly on VSCode. Remove old MSYS2 environment.

--- lib/Woopsi/woopsiExamples/demoWoopsi/arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
+++ lib/Woopsi/woopsiExamples/demoWoopsi/arm9/source/TGDSMemoryAllocator/TGDSMemoryAllocator.c
@@ -22,6 +22,7 @@ USA
 #include "posixHandleTGDS.h"
 #include "xmem.h"
 #include "dldi.h"
+#include "dsregs.h"
 
 ////////[For custom Memory Allocator implementation]:////////
 //You need to override getProjectSpecificMemoryAllocatorSetup():
@@ -31,7 +32,14 @@ USA
 	////////[Custom Memory implementation ]////////
 
 //Definition that overrides the weaksymbol expected from toolchain to init ARM9's TGDS memory allocation
-struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7MallocStartAddress, int ARM7MallocSize, bool isCustomTGDSMalloc, u32 TargetARM7DLDIAddress){
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7MallocStartAddress, int ARM7MallocSize, bool isCustomTGDSMalloc, u32 TargetARM7DLDIAddress) {
 	struct AllocatorInstance * customMemoryAllocator = &CustomAllocatorInstance;
 	memset((u8*)customMemoryAllocator, 0, sizeof(CustomAllocatorInstance));
 	customMemoryAllocator->customMalloc = isCustomTGDSMalloc;
@@ -39,7 +47,7 @@ struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7Malloc
 	customMemoryAllocator->ARM7MallocSize = ARM7MallocSize;
 	
 	customMemoryAllocator->ARM9MallocStartaddress = (u32)sbrk(0);
-	customMemoryAllocator->memoryToAllocate = (768*1024);	//512K Alloc (Woopsi objects)
+	customMemoryAllocator->memoryToAllocate = (768*1024);
 	customMemoryAllocator->CustomTGDSMalloc9 = (TGDSARM9MallocHandler)&Xmalloc;
 	customMemoryAllocator->CustomTGDSCalloc9 = (TGDSARM9CallocHandler)&Xcalloc;
 	customMemoryAllocator->CustomTGDSFree9 = (TGDSARM9FreeHandler)&Xfree;
@@ -56,5 +64,7 @@ struct AllocatorInstance * getProjectSpecificMemoryAllocatorSetup(u32 ARM7Malloc
 	customMemoryAllocator->DLDI9StartAddress = (u32)&_io_dldi_stub;
 	customMemoryAllocator->TargetARM7DLDIAddress = TargetARM7DLDIAddress;
 	
+	//Memory Setup: ARM7 TGDS 64K = 0x03800000 ~ 0x03810000. TGDS Sound Streaming code: Enabled
+	WRAM_CR = WRAM_32KARM9_0KARM7;
 	return customMemoryAllocator;
 }
