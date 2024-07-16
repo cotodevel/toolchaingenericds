ebc476d281726317c6018d8586a079aae5c06306
cotodevel <programmer.coto.1988@gmail.com>
2024-07-16 17:32:53 -0400

    -fixed ARGV protocol again.

--- src/templateCode/source/loader.h
+++ src/templateCode/source/loader.h
@@ -46,6 +46,7 @@ USA
 #define ARM7i_RAM_ADDRESS ((u32*)(TGDS_MB_V3_MEMBASE-(4*11)))
 #define ARM7i_BOOT_SIZE ((u32*)(TGDS_MB_V3_MEMBASE-(4*12)))
 #define TGDS_IS_TGDS_HOMEBREW ((u32*)(TGDS_MB_V3_MEMBASE-(4*13)))
+#define TGDS_ARGV_BUFFER ((u32*)(((int)0x02000000)-(512)))	//itcm
 
 //The base address all four ARM7/ARM9/ARM7i/ARM9i should have at least to be ran through TGDS-MB v3
 #define ARM_MININUM_LOAD_ADDR ((u32)0x01000000)
